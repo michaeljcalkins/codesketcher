fa8e09502e57dfdef787a94409d93caf31ba61ce
Michael Calkins <michaeljamescalkins@gmail.com>
2016-02-04 09:30:30 -0800

    fixes

--- src/main.js
+++ src/main.js
@@ -1,33 +1,5 @@
 'use strict'
 
-var pkg = require('../package.json') // Insert your app's manifest here
-var updater = require('../../electron-auto-updater')
-var upd = new updater(pkg)
-var copyPath, execPath
-
-// ------------- Step 1 -------------
-upd.checkNewVersion(function(error, newVersionExists, manifest) {
-    if (!error && newVersionExists) {
-
-        // ------------- Step 2 -------------
-        upd.download(function(error, filename) {
-            if (!error) {
-
-                // ------------- Step 3 -------------
-                upd.unpack(filename, function(error, newAppPath) {
-                    if (!error) {
-
-                        // ------------- Step 4 -------------
-                        upd.runInstaller(newAppPath, [upd.getAppPath(), upd.getAppExec()],{})
-                        app.quit()
-                    }
-                }, manifest)
-            }
-        }, manifest)
-    }
-})
-
-
 
 // Report crashes to our server.
 require('crash-reporter').start({
@@ -46,6 +18,55 @@ const BrowserWindow = electron.BrowserWindow
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
 let mainWindow
+
+
+//
+// var pkg = require('../package.json') // Insert your app's manifest here
+// var updater = require('../../electron-auto-updater')
+// var upd = new updater(pkg)
+// var copyPath, execPath
+//
+// // Args passed when new app is launched from temp dir during update
+// if(gui.App.argv.length) {
+//     // ------------- Step 5 -------------
+//     copyPath = gui.App.argv[0];
+//     execPath = gui.App.argv[1];
+//
+//     // Replace old app, Run updated app from original location and close temp instance
+//     upd.install(copyPath, function(err) {
+//         if(!err) {
+//
+//             // ------------- Step 6 -------------
+//             upd.run(execPath, null);
+//             gui.App.quit();
+//         }
+//     });
+// }
+// else { // if no arguments were passed to the app
+//     // ------------- Step 1 -------------
+//     upd.checkNewVersion(function(error, newVersionExists, manifest) {
+//         if (!error && newVersionExists) {
+//
+//             // ------------- Step 2 -------------
+//             upd.download(function(error, filename) {
+//                 if (!error) {
+//
+//                     // ------------- Step 3 -------------
+//                     upd.unpack(filename, function(error, newAppPath) {
+//                         if (!error) {
+//
+//                             // ------------- Step 4 -------------
+//                             upd.runInstaller(newAppPath, [upd.getAppPath(), upd.getAppExec()],{})
+//                             app.quit()
+//                         }
+//                     }, manifest)
+//                 }
+//             }, manifest)
+//         }
+//     })
+// }
+
+
 
 function createWindow () {
     // Create the browser window.
